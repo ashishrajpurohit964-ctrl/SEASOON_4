0459c444d425b6445db16fa76c386ca7fb203b2d
Ashish Rajpurohit <ashishrajpurohit964@gmail.com>
2026-06-17 14:23:56 +0530

    Add files via upload

---
+++ SEASOON_4.py
@@ -0,0 +1,32 @@
+"""<<<------------------------------------------------------------------------------SEASOON :--4-------------------------------------------------------------------------------------->>>"""
+
+
+'''1.Take the string 'Flipkart-Sale2024' and use string methods to convert it to lowercase, replace the dash with a space, and print the result'''
+# stri = "Flipkart-Sale2024"
+# print(stri.lower())
+# n = stri. lower(),stri.replace("-"," ")
+# print(n)
+
+'''2.Given the product name ' OnePlus Nord-CE 3 ', write code to clean it by removing extra spaces, converting all letters to uppercase, and 
+replacing the dash with a colon.<br><br><em><strong>Hint:</strong> Use strip(), upper(), and replace() methods in sequence.</em>.'''
+# product = "Onepluse Node-CE 3"
+# n = product.strip().upper().replace("-",":")
+# print(n)
+
+
+'''3.Write a function split_product_code(product_code) that takes a string like 'ZOMATO-FOOD-2024' and returns a list of its parts using the split() method'''
+# def spli_product_code(product_code):
+#     return product_code.split("-")
+# product_code = "ZOMATO-FOOD-2024"
+# result = spli_product_code(product_code)
+# print(result)
+
+'''4.Given the string 'Spotify_Premium_Offer', use string slicing to extract and print only the word 'Premium'.'''
+# s = "spotify_premium_offer"
+# print(s[8:15])
+
+
+'''5.Format and print a message using variables: product = 'Myntra Shirt', price = 799.5. The output should be: 'Deal: Myntra Shirt is available at ₹799.50 only!' using string formatting.'''
+# product =   "Myntra shirt"
+# price = 799.5
+# print(f"Deal: {product} is available at {price}")
