d4fe32f368653f555b03313f24284ea3ddb382aa
Ashish Rajpurohit <ashishrajpurohit964@gmail.com>
2026-06-30 12:13:47 +0530

    Add files via upload

---
+++ seasoon2.py
@@ -0,0 +1,32 @@
+'''<<-------------------------------------------------------------------------seasoon_2--------------------------------------------------------------------------------------------->>>'''
+
+
+'''1.Create three variables in Python named item_price, delivery_fee, and coupon_discount, and assign them values representing a Swiggy order (e.g., 250, 30, 20). Print each variable.'''
+iteam_price = 250
+delivery_fees = 30
+coupon_discount = 20
+print("The price of the iteams is :--",iteam_price)
+print("The fees of the delivery is :--",delivery_fees)
+print("The discount of the coupon is :--",coupon_discount)
+
+'''2.Write a Python script that uses indentation correctly to check if a Flipkart product's price is above 1000; if yes, print 'Eligible for free delivery', else print 'Delivery charges apply'.
+<br><br><em><strong>Hint:</strong> Use an if-else block and make sure your indentation is consistent.</em>.'''
+product_price=int(input("enter your product price:--"))
+if product_price >1000:
+    print("Eligible for free delivery")
+else:
+    print("Delivery charges apply")
+
+'''3. Add both single-line and multi-line comments in your script explaining what each section does, using # for single-line and triple quotes for multi-line comments.'''
+# we use single line to explain the logic behind the code
+'''we use multi line comment to explain the defination or long text in python'''
+
+'''4.Create variables for an IPL match: team_name, runs_scored, and overs_played. Assign appropriate values and print them using the correct naming conventions for Python variables.
+<br><br><em><strong>Constraint:</strong> Use snake_case for all variable names.</em>.'''
+team_name = "mumbai indians"
+runs_scored = 186
+over_played = 15
+print("The name of the team :--",team_name)
+print("The runs scored by this team are :--",runs_scored)
+print("The over played by this team are  :--",over_played)
+
